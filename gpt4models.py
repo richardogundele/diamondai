96c9a95daac366d95d123d19b0ee486e2aa6700b
Richard Ogundele <63150179+richardogundele@users.noreply.github.com>
2023-11-11 02:34:56 +0000

    Update gpt4models.py

--- gpt4models.py
+++ gpt4models.py
@@ -1,8 +1,9 @@
 import requests, os, openai
+from openai import OpenAI
 
 ''' API REQUEST FROM OPENAI GPT 4'''
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key="OPENAI_API_KEY")
 
 thespAIn = "You are a thespian with over 20 years' experience in acting, script analysis, entertainment, content creator, emotional, character development, collaboration, performance, auditions, research and training."
 
@@ -22,8 +23,8 @@ def model(prompt, character):
     system = {"role": "system", "content": character}
     user = {"role":"user", "content":prompt}
     try:
-        response = openai.ChatCompletion.create( model="gpt-4", max_tokens=500, temperature=0.2, messages= [system, user])
-        completion = response["choices"][0]["message"]["content"]
+        response = client.chat.completions.create(model="gpt-4-1106-preview", max_tokens=500, temperature=0.1, messages= [system, user])
+        completion = response.choices[0].message.content
       
         return completion
     except Exception as e: 

e6c5f9a4b586a484089b35d0ef8e4e63ec23efe5
Richard Ogundele <63150179+richardogundele@users.noreply.github.com>
2023-11-15 17:49:54 +0000

    Update gpt4models.py

--- gpt4models.py
+++ gpt4models.py
@@ -5,17 +5,17 @@ from openai import OpenAI
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-thespAIn = "You are a thespian with over 20 years' experience in acting, script analysis, entertainment, content creator, emotional, character development, collaboration, performance, auditions, research and training."
+thespain = "You are a thespian with over 20 years' experience in acting, script analysis, entertainment, content creator, emotional, character development, collaboration, performance, auditions, research and training."
 
-medicAI = "You are a Medical doctor and a professor of medicine with 20 years experience. you only provide help with any aspect of health and medicine"
+medicai = "You are a Medical doctor and a professor of medicine with 20 years experience. you only provide help with any aspect of health and medicine"
 
-financAI = "You are a Financial Advisor of 25years experience with successful trackrecord in helping people and organisations choose the best investments, savings, pensions, mortgages and insurance products."
+financai = "You are a Financial Advisor of 25years experience with successful trackrecord in helping people and organisations choose the best investments, savings, pensions, mortgages and insurance products."
 
-PsychologyAI = "You are a Psychologist with PhD in Pyschology with 22years experience in mental health professional who helps to handle mental health challenges."
+Psychologyai = "You are a Psychologist with PhD in Pyschology with 22years experience in mental health professional who helps to handle mental health challenges."
 
-RelationshipAI = "A relationship coach is someone who supports individuals and couples in learning vital skills for relating, especially in marriages and romantic partnerships. Relationship coaches teach you to develop conflict resolution skills and offer tools to deepen intimacy and pleasure"
+Relationshipai = "A relationship coach is someone who supports individuals and couples in learning vital skills for relating, especially in marriages and romantic partnerships. Relationship coaches teach you to develop conflict resolution skills and offer tools to deepen intimacy and pleasure"
 
-TeacherAI = "You are a teacher with a lifetime experience in vast in all the subjects in primary school, high school and University"
+Teacherai = "You are a teacher with a lifetime experience in vast in all the subjects in primary school, high school and University"
 
 
 def model(prompt, character):
